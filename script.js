5ae5f374cad33709ebe3541d915f8c1b05dcb86f
crazy_simple <ojbekmuslimov1@gmail.com>
2025-01-21 16:47:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,347 @@
+function orderSet() {
+    const pricePerSet = 45000;
+    let set = parseInt(prompt("Qancha Qiyqiriq Set buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert(`1 ta Qiyqiriq Set narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert(`2 ta Qiyqiriq Set narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert(`3 ta Qiyqiriq Set narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert(`4 ta Qiyqiriq Set narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+            case false:
+                alert('okey');
+                break;
+
+        default:
+            alert("Buncha Qiyqiriq Set zakaz qilolmaysiz");
+            break;
+    }
+
+    alert("Buyurtma qabul qilindi!");
+}
+
+
+
+
+function orderSet2() {
+    const pricePerSet = 79000;
+    const set = parseInt(prompt("Qancha Снек сет buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert(`1 ta Снек сет narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert(`2 ta Снек сет narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert(`3 ta Снек сет narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert(`4 ta Снек сет narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Снек сет zakaz qilolmaysiz");
+            break;
+    }
+
+    alert("Buyurtma qabul qilindi!");
+}
+
+
+
+
+
+
+function orderSet3() {
+    const pricePerSet = 34000;
+    const set = parseInt(prompt("Qancha Классик сет buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert = confirm(`1 ta Классик сет narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert = confirm(`2 ta Классик сет narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Qiyqiriq SetКлассик сет narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Классик сет narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Классик сет zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
+
+
+
+
+
+
+function orderSet4() {
+    const pricePerSet = 45000 ;
+    const set = parseInt(prompt("Qancha Лонгер рингс сет buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert = confirm(`1 ta Лонгер рингс сет narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert = confirm(`2 ta Лонгер рингс сет narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Лонгер рингс сет narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Лонгер рингс сет narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Лонгер рингс сет zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
+
+
+
+
+
+
+
+function orderSet5() {
+    const pricePerSet = 62000;
+    const set = parseInt(prompt("Qancha Биг сет buyurtma qilmoqchisiz?"));
+
+switch (set) {
+        case 1:
+            alert = confirm(`1 ta Биг сет narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert = confirm(`2 ta Биг сет narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Биг сет narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Биг сет narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Биг сет zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
+
+
+
+
+
+function orderSet6() {
+    const pricePerSet = 53000;
+    const set = parseInt(prompt("Qancha Лестер сет buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert = confirm(`1 ta Лестер сет narxi: ${pricePerSet.toLocaleString()} so'm`);
+            brea
+        case 2:
+            alert = confirm(`2 ta Лестер сет narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Лестер сет narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Лестер сет narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Лестер сет zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
+
+
+
+
+
+function orderSet7() {
+    const pricePerSet = 36000;
+    const set = parseInt(prompt("Qancha Скул сет buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert = confirm(`1 ta Скул сет narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert = confirm(`2 ta Скул сет narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Скул сет narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Скул сет narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Скул сет zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
+
+
+
+
+
+function orderSet8() {
+    const pricePerSet = 20000;
+    const set = parseInt(prompt("Qancha Комбо сет Большой buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert = confirm(`1 ta Комбо сет Большой narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert = confirm(`2 ta Комбо сет Большой narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Комбо сет Большой narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Комбо сет Большой narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Комбо сет Большой -zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
+
+
+
+
+function orderSet9() {
+    const pricePerSet = 43000;
+    const set = parseInt(prompt("Qancha Do’stlar 1х buyurtma qilmoqchisiz?"));
+
+switch (set) {
+        case 1:
+            alert = confirm(`1 ta Do’stlar 1х narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert = confirm(`2 ta Do’stlar 1х narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Do’stlar 1х narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Do’stlar 1х narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Do’stlar 1х zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
+
+
+
+
+function orderSet10() {
+    const pricePerSet = 79000;
+    const set = parseInt(prompt("Qancha Do’stlar 2х buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert = confirm(`1 ta Do’stlar 2х narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert = confirm(`2 ta Do’stlar 2х narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Do’stlar 2х narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Do’stlar 2х narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Do’stlar 2х zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
+
+
+
+
+function orderSet11() {
+    const pricePerSet = 149000;
+    const set = parseInt(prompt("Qancha Do’stlar 4х buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert = confirm(`1 ta Do’stlar 4х narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert = confirm(`2 ta Do’stlar 4х narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Do’stlar 4х narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Do’stlar 4х narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Do’stlar 4х zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
+
+
+
+
+
+function orderSet12() {
+    const pricePerSet = 43000;
+    const set = parseInt(prompt("Qancha Острый Do’stlar 1х buyurtma qilmoqchisiz?"));
+
+    switch (set) {
+        case 1:
+            alert = confirm(`1 ta Острый Do’stlar 1х narxi: ${pricePerSet.toLocaleString()} so'm`);
+            break;
+        case 2:
+            alert = confirm(`2 ta Острый Do’stlar 1х narxi: ${(2 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 3:
+            alert = confirm(`3 ta Острый Do’stlar 1х narxi: ${(3 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        case 4:
+            alert = confirm(`4 ta Острый Do’stlar 1х narxi: ${(4 * pricePerSet).toLocaleString()} so'm`);
+            break;
+        default:
+            alert("Buncha Острый Do’stlar 1х zakaz qilolmaysiz");
+            break;
+    }
+
+    alert = confirm("Buyurtma qabul qilindi!");
+}
